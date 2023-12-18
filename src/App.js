e1fc3ef00af2f6b046d475fb210ddc19c493e75f
Zaher Madi <zahermadi@yahoo.fr>
2023-12-18 20:03:19 +0100

    react-router-dom, addded

--- src/App.js
+++ src/App.js
@@ -5,19 +5,34 @@ import Logo from './Common/Header/Logo/Logo';
 import PokemonsList from './PokemonsList/PokemonsList';
 import Searchbar from './PokemonsList/Toolbar/Toolbar';
 
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider
+} from "react-router-dom";
+
 
 function App() {
   const [values, setValues] = React.useState("")
   
-
-
-  return (
-
-    <div>
-      <Header/>
-      <Searchbar values={values} setValues={setValues}/>
-      <PokemonsList values ={values}/>
-     </div>
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route path="/" element={
+              <div>
+              <Header/>
+              <Searchbar values={values} setValues={setValues}/>
+              <PokemonsList values ={values}/>
+             </div>
+        } />
+      </>
+    )
   );
+
+  return ( <RouterProvider router={router}/>
+
+  );
+
 }
 export default App;
